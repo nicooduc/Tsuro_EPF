ebdbab364b5c849d424d43d92258c346a5a328f8
nykol <nykol@DESKTOP-MFU2D5A.home>
2020-12-11 13:05:48 +0100

    Def tuiles

--- src/tsuro/Tuile.java
+++ src/tsuro/Tuile.java
@@ -11,4 +11,109 @@ package tsuro;
  */
 public class Tuile {
     
+    public void tile0() {
+        int[] tab0 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile1() {
+        int[] tab1 = {6,3,2,5,4,1,8,7};
+    }
+    public void tile2() {
+        int[] tab2 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile3() {
+        int[] tab3 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile4() {
+        int[] tab4 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile5() {
+        int[] tab5 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile6() {
+        int[] tab6 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile7() {
+        int[] tab7 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile8() {
+        int[] tab8 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile9() {
+        int[] tab9 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile10() {
+        int[] tab10 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile11() {
+        int[] tab11 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile12() {
+        int[] tab12 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile13() {
+        int[] tab13 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile14() {
+        int[] tab14 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile15() {
+        int[] tab15 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile16() {
+        int[] tab16 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile17() {
+        int[] tab17 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile18() {
+        int[] tab18 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile19() {
+        int[] tab19 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile20() {
+        int[] tab20 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile21() {
+        int[] tab21 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile22() {
+        int[] tab22 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile23() {
+        int[] tab23 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile24() {
+        int[] tab24 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile25() {
+        int[] tab25 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile26() {
+        int[] tab26 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile27() {
+        int[] tab27 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile28() {
+        int[] tab28 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile29() {
+        int[] tab29 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile30() {
+        int[] tab30 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile31() {
+        int[] tab31 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile32() {
+        int[] tab132 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile33() {
+        int[] tab33 = {7,4,6,2,8,3,1,5};
+    }
+    public void tile34() {
+        int[] tab34 = {7,4,6,2,8,3,1,5};
+    }
 }
