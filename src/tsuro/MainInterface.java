7596672dfb0002ee96a6b08f8da49878523072e7
nykol <nykol@LAPTOP-PENDS0C9>
2020-12-30 15:39:34 +0100

    Ajout place joueurs

--- src/tsuro/MainInterface.java
+++ src/tsuro/MainInterface.java
@@ -21,6 +21,7 @@ public class MainInterface extends javax.swing.JFrame {
      */
     public MainInterface() {
         initComponents();
+        CartesJoueur.setVisible(false);
         
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 6; j++) {
@@ -58,6 +59,28 @@ public class MainInterface extends javax.swing.JFrame {
         PlateauJeu = new javax.swing.JPanel();
         Pioche = new javax.swing.JPanel();
         CartesJoueur = new javax.swing.JPanel();
+        Panneau_start = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        Nbr_Joueurs = new javax.swing.JTextField();
+        Btn_init1 = new javax.swing.JButton();
+        Panneau_joueurs = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        Nom_Joueur_Noir = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        Nom_Joueur_Rouge = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        Nom_Joueur_Vert = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
+        Nom_Joueur_Jaune = new javax.swing.JTextField();
+        jLabel6 = new javax.swing.JLabel();
+        Nom_Joueur_Bleu = new javax.swing.JTextField();
+        jLabel7 = new javax.swing.JLabel();
+        Nom_Joueur_Violet = new javax.swing.JTextField();
+        jLabel8 = new javax.swing.JLabel();
+        Nom_Joueur_Cyan = new javax.swing.JTextField();
+        jLabel9 = new javax.swing.JLabel();
+        Nom_Joueur_Blanc = new javax.swing.JTextField();
+        Btn_init2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new java.awt.Dimension(1280, 800));
@@ -69,21 +92,79 @@ public class MainInterface extends javax.swing.JFrame {
 
         Pioche.setBackground(new java.awt.Color(204, 255, 255));
         Pioche.setPreferredSize(new java.awt.Dimension(130, 130));
-
-        javax.swing.GroupLayout PiocheLayout = new javax.swing.GroupLayout(Pioche);
-        Pioche.setLayout(PiocheLayout);
-        PiocheLayout.setHorizontalGroup(
-            PiocheLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 130, Short.MAX_VALUE)
-        );
-        PiocheLayout.setVerticalGroup(
-            PiocheLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 130, Short.MAX_VALUE)
-        );
+        Pioche.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         CartesJoueur.setBackground(new java.awt.Color(204, 255, 255));
         CartesJoueur.setPreferredSize(new java.awt.Dimension(390, 130));
         CartesJoueur.setLayout(new java.awt.GridLayout(1, 3));
+
+        Panneau_start.setBackground(new java.awt.Color(255, 255, 204));
+        Panneau_start.setPreferredSize(new java.awt.Dimension(314, 130));
+        Panneau_start.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setText("Nombre de joueurs :");
+        Panneau_start.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 30, -1, -1));
+
+        Nbr_Joueurs.setText("jTextField1");
+        Panneau_start.add(Nbr_Joueurs, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 30, -1, -1));
+
+        Btn_init1.setText("GO !");
+        Panneau_start.add(Btn_init1, new org.netbeans.lib.awtextra.AbsoluteConstraints(105, 73, 110, 40));
+
+        Panneau_joueurs.setBackground(new java.awt.Color(255, 255, 204));
+        Panneau_joueurs.setPreferredSize(new java.awt.Dimension(462, 190));
+        Panneau_joueurs.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel2.setText("Joueur Noir");
+        Panneau_joueurs.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 20, -1, -1));
+
+        Nom_Joueur_Noir.setText("jTextField1");
+        Panneau_joueurs.add(Nom_Joueur_Noir, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 20, 80, -1));
+
+        jLabel3.setText("Joueur Rouge");
+        Panneau_joueurs.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 50, -1, -1));
+
+        Nom_Joueur_Rouge.setText("jTextField1");
+        Panneau_joueurs.add(Nom_Joueur_Rouge, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 50, 80, -1));
+
+        jLabel4.setText("Joueur Vert");
+        Panneau_joueurs.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 80, -1, -1));
+
+        Nom_Joueur_Vert.setText("jTextField1");
+        Panneau_joueurs.add(Nom_Joueur_Vert, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 80, 80, -1));
+
+        jLabel5.setText("Joueur Jaune");
+        Panneau_joueurs.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 110, -1, -1));
+
+        Nom_Joueur_Jaune.setText("jTextField1");
+        Panneau_joueurs.add(Nom_Joueur_Jaune, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 110, 80, -1));
+
+        jLabel6.setText("Joueur Bleu");
+        Panneau_joueurs.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 20, -1, -1));
+
+        Nom_Joueur_Bleu.setText("jTextField1");
+        Panneau_joueurs.add(Nom_Joueur_Bleu, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 20, 80, -1));
+
+        jLabel7.setText("Joueur Violet");
+        Panneau_joueurs.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 50, -1, -1));
+
+        Nom_Joueur_Violet.setText("jTextField1");
+        Panneau_joueurs.add(Nom_Joueur_Violet, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 50, 80, -1));
+
+        jLabel8.setText("Joueur Cyan");
+        Panneau_joueurs.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 80, -1, -1));
+
+        Nom_Joueur_Cyan.setText("jTextField1");
+        Panneau_joueurs.add(Nom_Joueur_Cyan, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 80, 80, -1));
+
+        jLabel9.setText("Joueur Blanc");
+        Panneau_joueurs.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 110, -1, -1));
+
+        Nom_Joueur_Blanc.setText("jTextField1");
+        Panneau_joueurs.add(Nom_Joueur_Blanc, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 110, 80, -1));
+
+        Btn_init2.setText("GO !");
+        Panneau_joueurs.add(Btn_init2, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 140, 110, 40));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -93,22 +174,31 @@ public class MainInterface extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(PlateauJeu, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(CartesJoueur, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(49, 49, 49))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(18, 18, 18)
-                        .addComponent(Pioche, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 51, Short.MAX_VALUE)
-                        .addComponent(CartesJoueur, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(49, 49, 49))))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(Panneau_joueurs, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(Pioche, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(Panneau_start, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addContainerGap())))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(Pioche, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(Pioche, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(Panneau_start, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(18, 18, 18)
+                        .addComponent(Panneau_joueurs, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(CartesJoueur, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addComponent(PlateauJeu, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -154,10 +244,34 @@ public class MainInterface extends javax.swing.JFrame {
             }
         });
     }
+    
+    // Fonctions à placer ici
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Btn_init1;
+    private javax.swing.JButton Btn_init2;
     private javax.swing.JPanel CartesJoueur;
+    private javax.swing.JTextField Nbr_Joueurs;
+    private javax.swing.JTextField Nom_Joueur_Blanc;
+    private javax.swing.JTextField Nom_Joueur_Bleu;
+    private javax.swing.JTextField Nom_Joueur_Cyan;
+    private javax.swing.JTextField Nom_Joueur_Jaune;
+    private javax.swing.JTextField Nom_Joueur_Noir;
+    private javax.swing.JTextField Nom_Joueur_Rouge;
+    private javax.swing.JTextField Nom_Joueur_Vert;
+    private javax.swing.JTextField Nom_Joueur_Violet;
+    private javax.swing.JPanel Panneau_joueurs;
+    private javax.swing.JPanel Panneau_start;
     private javax.swing.JPanel Pioche;
     private javax.swing.JPanel PlateauJeu;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     // End of variables declaration//GEN-END:variables
 }
