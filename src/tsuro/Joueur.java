e6ddccba46cf403de8addc96ebcc3a42d86ea2d8
nykol <nykol@DESKTOP-MFU2D5A.home>
2021-01-09 17:01:32 +0100

    Test mélange pioche

--- src/tsuro/Joueur.java
+++ src/tsuro/Joueur.java
@@ -14,13 +14,14 @@ public class Joueur {
     String Nom; //nom joueur
     String Couleur; //couleur joueur
     Tuile[] TuileJ = new Tuile[3]; //place main de tuiles du joueur
-    int nbrTour = 0;
+    int nbrTour;
     Pion PionJ;
     
     public Joueur(String nom, String couleur) {
         Nom = nom;
         Couleur = couleur;
         PionJ = new Pion(Couleur); //pion joueur
+        nbrTour = 0;
     }
     
     public void TourPasse() {
